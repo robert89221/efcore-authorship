ff92f698dd3fd8dc132185de3fbcd1ca79188c88
Robert <robert89221@outlook.com>
2024-01-08 14:32:55 +0100

    Add and list entries

--- ef-core-authorship-test/Model.cs
+++ ef-core-authorship-test/Model.cs
@@ -30,34 +30,33 @@ namespace ef_core_authorship_test
     public int Year { get; set; }
     public int Pages { get; set; }
 
+    //  för junction-tabellen
     public List<Author> Authors { get; set; }
 
     //  EF core kräver en vanlig eller primär ctor i detta formatet, med argument som har samma namn som kolumnerna i tabellen
-    public Book(string Title, string Category, int Year, int Pages) => (this.Title, this.Category, this.Year, this.Pages, this.Authors) = (Title, Category, Year, Pages, []);
+    public Book(string Title="", string Category="", int Year=0, int Pages=0) => (this.Title, this.Category, this.Year, this.Pages, this.Authors) = (Title, Category, Year, Pages, []);
 
     //  övriga ctors kan läggas till för bekvämlighet
-
-    public Book() => (Title, Category, Year, Pages, Authors) = ("", "", 0, 0, []);
-
-    public Book(string t, string c, int y, int p, params string[] a)
-    {
-      (Title, Category, Year, Pages, Authors) = (t, c, y, p, []);
-      foreach (var auth in a)    Authors.Add(new Author(auth));
-    }
-
-    public Book(string t, string c, int y, int p, List<Author> a) => (Title, Category, Year, Pages, Authors) = (t, c, y, p, a);
+    public Book(string t, string c, int y, int p, params Author[] a) => (Title, Category, Year, Pages, Authors) = (t, c, y, p, a.ToList());
 
     public override string ToString()  =>  $"{Title} ({Year}), {Category}, {Pages} pages";
   }
 
 
-  internal class Author(string Name)
+  internal class Author
   {
     public int Id { get; set; }
 
-    public string Name { get; set; } = Name;
+    public string Name { get; set; }
 
-    public List<Book> Books { get; set; } = [];
+    //  för junction-tabellen
+    public List<Book> Books { get; set; }
+
+    public Author(string Name = "") => (this.Name, this.Books) = (Name, []);
+
+    public Author(string Name, params Book[] b) => (this.Name, this.Books) = (Name, b.ToList());
+
+    public override string ToString() => Name;
   }
 
 }
--- ef-core-authorship-test/Program.cs
+++ ef-core-authorship-test/Program.cs
@@ -6,19 +6,27 @@ using System.ComponentModel;
 
 var db = new AuthorshipDbContext();
 
-DeleteAll();
-AddEntries();
+//DeleteAll();
+//AddEntries();
 
-
+ListAll();
 
 
 
 
 void ListAll()
 {
+  Console.WriteLine("Authors:");
+  foreach (var auth in db.Authors.Include("Books"))    Console.WriteLine($"  {auth}, {auth.Books.Count} titles");
+  Console.WriteLine();
 
+  Console.WriteLine("Books:");
+  foreach (var book in db.Books.Include("Authors"))
+  {
+    var auths = String.Join(", ", book.Authors);
+    Console.WriteLine($"  {book}, ({auths})");
+  }
 }
-
 
 void DeleteAll()
 {
@@ -29,7 +37,31 @@ void DeleteAll()
 
 void AddEntries()
 {
-  db.Books.Add(new Book("boken", "skönlit", 2001, 55, "auth 1", "auth2", "auth3"));
-  db.SaveChanges();
+  db.Books.Add(new Book("A Horse's Tale", "Novel", 1907, 153, new Author("Mark Twain")));
+  db.Books.Add(new Book("The Hidden Child", "Novel", 2011, 400, new Author("Camilla Läckberg")));
+  db.Books.Add(new Book("Parting Breath", "Novel", 1977, 200, new Author("Catherine Aird")));
+  db.Books.Add(new Book("The Day Will Come", "Novel", 2007, 250, new Author("Judy Clemens")));
 
+  var steve = new Author("Steve McConnell");
+  db.Authors.Add(steve);
+  db.Books.Add(new Book("Professional Software Development", "Software", 2003, 272, steve));
+  db.Books.Add(new Book("Code Complete", "Software", 2004, 952, steve));
+  db.Books.Add(new Book("Rapid Development", "Software", 1996, 672, steve));
+
+  var john = new Author("John Ousterhout");
+  var ken = new Author("Ken Jones");
+  db.Authors.Add(john);
+  db.Authors.Add(ken);
+  db.Books.Add(new Book("Philosophy of Software Design", "Software", 2021, 196, john));
+  db.Books.Add(new Book("TCL and the TK Toolkit", "Software", 2009, 816, john, ken));
+
+  var titus = new Author("Titus Winters");
+  var tom = new Author("Tom Manshreck");
+  var hyrum = new Author("Hyrum Wright");
+  db.Authors.Add(titus);
+  db.Authors.Add(tom);
+  db.Authors.Add(hyrum);
+  db.Books.Add(new Book("Software Engineering at Google", "Software", 2020, 599, titus, tom, hyrum));
+
+  db.SaveChanges();
 }
